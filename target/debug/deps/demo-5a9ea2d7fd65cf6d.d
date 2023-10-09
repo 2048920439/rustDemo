3cea92d5d3e2a640f7ea5b27e1fa54285965da03
付忠源 <2048920439@qq.com>
2023-10-09 17:35:15 +0800

    remove target

--- target/debug/deps/demo-5a9ea2d7fd65cf6d.d
+++
@@ -1,5 +0,0 @@
-D:\rust\demo\target\debug\deps\demo-5a9ea2d7fd65cf6d.rmeta: src\main.rs
-
-D:\rust\demo\target\debug\deps\demo-5a9ea2d7fd65cf6d.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/demo-980f38a211b875db.d
+++
@@ -1,5 +0,0 @@
-D:\rust\demo\target\debug\deps\demo-980f38a211b875db.rmeta: src\main.rs
-
-D:\rust\demo\target\debug\deps\demo-980f38a211b875db.d: src\main.rs
-
-src\main.rs:
